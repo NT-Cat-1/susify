0163599760d11318db32da80e48976d0ef24c7b6
NT-Cat-1 <69440080+NT-Cat-1@users.noreply.github.com>
2021-04-13 12:33:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; //constant for xhr
-const DomParser = require('dom-parser'); //constant for domparse
-var parser = new DomParser();
+//const DomParser = require('dom-parser'); //constant for domparse
+//var parser = new DomParser();
 var http = require("http");
 var app = require("express")(http);
 var xhr = new XMLHttpRequest();
@@ -29,51 +29,7 @@ app.get("/susify",function(req, res){
 	xhr.open("GET",req.query.prot+"://"+req.query.adr);
 	xhr.responseType = "text";
 	xhr.onload = function(){
-        var document = parser.parseFromString(xhr.responseText);
-        var list = document.getElementsByTagName("img");
-        var flist = document.getElementsByTagName("iframe");
-        var llist = document.getElementsByTagName("a");
-        var slist = document.getElementsByTagName("source");
-        var elist = document.getElementsByTagName("yt-formatted-string");
-        try{
-        document.getElementById("country-code").innerText="SUS";
-        }catch{}
-        try{
-        document.getElementById("notification-count").innerText="SUS";
-        }catch{}
-        try{
-        document.getElementById("ytd-thumbnail-overlay-time-status-renderer").innerText="S:US";
-        }catch{}
-        try{
-        for (i in list){
-        list[i].src="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";
-        list[i].srcset="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";
-        }
-        }catch{}
-        try{
-        for (i in flist){
-        flist[i].src="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";
-        }
-        }catch{}
-        try{
-        for (i in llist){
-        //if (isNaN(parseInt(i))){return;}
-        llist[i].href="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";
-        }
-        }catch{}
-        try{
-        for (i in slist){
-        //if (isNaN(parseInt(i))){return;}
-        slist[i].srcset="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";
-        }
-        }catch{}
-        try{
-        for (i in elist){
-        //if (isNaN(parseInt(i))){return;}
-        elist[i].innerText="When the imposter is sus.";
-        }
-        }catch{}
-		res.send(document.rawHTML);
+           res.send(xhr.responseText+'<script>var list=document.getElementsByTagName("img");var flist=document.getElementsByTagName("iframe");var llist=document.getElementsByTagName("a");var slist=document.getElementsByTagName("source");var elist=document.getElementsByTagName("yt-formatted-string");try{document.getElementById("country-code").innerText="SUS";}catch{};try{document.getElementById("notification-count").innerText="SUS";}catch{};try{document.getElementById("ytd-thumbnail-overlay-time-status-renderer").innerText="S:US";}catch{};try{for(i in list){list[i].src="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";list[i].srcset="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";};}catch{};try{for(i in flist){flist[i].src="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";};}catch{};try{for(i in llist){llist[i].href="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";};}catch{};try{for(i in slist){slist[i].srcset="https://dl.dropbox.com/s/3y3qukshsjxzx5e/sus.jpg?dl=1";};}catch{};try{for(i in elist){elist[i].innerText="When the imposter is sus.";};}catch{};</script>');
 	};
 	try{xhr.send();}catch{res.status(500).send("Load failure.");}
 });
